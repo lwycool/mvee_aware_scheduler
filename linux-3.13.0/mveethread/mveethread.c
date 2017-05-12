62516667b3e4a2d9855b0af192fd18806e835485
prabhu <rajasekp@uci.edu>
2017-05-12 16:53:06 -0700

    Simplified the mveethread syscall signature since pid_t is a wellknown datastructure in both kernel and userspace.

--- linux-3.13.0/mveethread/mveethread.c
+++ linux-3.13.0/mveethread/mveethread.c
@@ -10,7 +10,7 @@ struct slave_thread {
     pid_t slave_pid;
 };
 
-asmlinkage long sys_mveethread(pid_t master_pid, void* slave_pids, size_t size, int len) {
+asmlinkage long sys_mveethread(pid_t master_pid, pid_t* slave_pids, int len) {
     //@joe - master_pid here is the thread/task id of the master thread and list of slave_pids will contain one or more related slave thread/task ids
     printk("Received data for mvee \n");
     struct slave_thread *tmp;
@@ -18,9 +18,7 @@ asmlinkage long sys_mveethread(pid_t master_pid, void* slave_pids, size_t size, 
     INIT_LIST_HEAD(&slave_pids_list.list);
     for (int i = 0; i < len; i++)
     {
-        void* ptr = (slave_pids + (i*size));
-        pid_t thispid = *(pid_t*)ptr;
-        &tmp->slave_pid=thispid;
+        &tmp->slave_pid=slave_pids[i];
         list_add(&(tmp->list), &(slave_pids_list.list));
     }
     //$TODO save the master_pid and the slave_pids_list which is associated with this master_pid into scheduler
