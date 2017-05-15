b912a550092dea511bb949e92384d489e1811baa
prabhu <rajasekp@uci.edu>
2017-05-15 13:37:03 -0700

    Using kmalloc to construct the slave task ids list in the kernel module.

--- linux-3.13.0/mveethread/mveethread.c
+++ linux-3.13.0/mveethread/mveethread.c
@@ -1,6 +1,7 @@
 #include <linux/kernel.h>
 #include <linux/types.h>
 #include <linux/list.h>
+#include <linux/slab.h>
 
 /*-----------------------------------------------------------------------------
     struct for associated mvee thread ids which will be used by the scheduler
@@ -12,15 +13,23 @@ struct slave_thread {
 
 asmlinkage long sys_mveethread(pid_t master_pid, pid_t* slave_pids, int len) {
     //@joe - master_pid here is the thread/task id of the master thread and list of slave_pids will contain one or more related slave thread/task ids
-    printk("Received data for mvee \n");
+    int i;
     struct slave_thread *tmp;
     struct slave_thread slave_pids_list;
+    printk("Received data for mvee \n");
+    printk("[[%d]] is the master pid", master_pid);
     INIT_LIST_HEAD(&slave_pids_list.list);
-    for (int i = 0; i < len; i++)
+    printk("inited slave pids list");
+    for (i = 0; i < len; i++)
     {
-        &tmp->slave_pid=slave_pids[i];
+        tmp= (struct slave_thread *)kmalloc(sizeof(struct slave_thread), GFP_KERNEL);
+        printk("returned from malloc");
+        tmp->slave_pid=slave_pids[i];
+        printk("assigned slave pid to tmp object");
         list_add(&(tmp->list), &(slave_pids_list.list));
+        printk("added tmp object to the list");
     }
+    printk("The loop has finished. Returning from syscall");
     //$TODO save the master_pid and the slave_pids_list which is associated with this master_pid into scheduler
     return 0;
 }
